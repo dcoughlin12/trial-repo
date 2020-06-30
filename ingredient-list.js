0a0fa6b322cab3135410303db5da8cc9474e4b9a
Devin Coughlin <devin_coughlin@hotmail.com>
2020-06-30 17:23:36 +0000

    while and for loops

---
+++ ingredient-list.js
@@ -0,0 +1,16 @@
+const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+
+// Write a while loop that prints out the contents of ingredients:
+
+for (let i = 0; i < ingredients.length; i++){
+    console.log(ingredients[i])
+}
+let index = 0
+while (index < ingredients.length){
+    console.log(ingredients[index]);
+    index++;
+}
+
+for (let i = 7; i >= 0; i--){
+    console.log(ingredients[i])
+}
